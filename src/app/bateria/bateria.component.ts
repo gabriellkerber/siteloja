17b225ec44c5db4b13ac6339dd0dffd4c0a20260
Gabriellkerber <gabriellkerber@bol.com.br>
2020-03-25 18:45:17 -0300

    commit

--- src/app/bateria/bateria.component.ts
+++ src/app/bateria/bateria.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Bateria } from '../models/bateria.model';
 
 @Component({
   selector: 'app-bateria',
@@ -7,9 +8,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BateriaComponent implements OnInit {
 
+  baterias: Bateria[] = [];
+
   constructor() { }
 
   ngOnInit(): void {
+
+    this.baterias = [
+
+      {
+        id: 1,
+        urlImagem:'assets/img/batg4.jpg',
+        titulo:'Bateria Moto G4 Plus',
+        descricaoCurta: 'Bateria Smartphone Motorola G4 Plus',
+        descricaoCompleta: '',
+        valor: 'R$ 65,00',
+      },
+      {
+        id: 2,
+        urlImagem:'assets/img/batz2.jpg',
+        titulo:'Bateria Moto Z2 Play',
+        descricaoCurta: 'Bateria Smartphone Motorola Z2 Play',
+        descricaoCompleta: '',
+        valor: 'R$ 90,00',
+      }
+    ]
   }
 
 }
